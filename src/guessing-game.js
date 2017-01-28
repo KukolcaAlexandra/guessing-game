feaaabadddc629ba91b177878ca4707250490779
KukolcaAlexandra <Kukolca_shura@mail.ru>
2017-01-28 14:23:22 +0300

    guessing-game/feature: implement GuessingGame class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,26 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+    	this.min = min;
+    	this.max = max;
     }
 
     guess() {
-
+    	
+    	this.res = this.min + Math.round((this.max - this.min)/2);
+    	return this.res;
     }
 
     lower() {
-
+    	
+    	this.max = this.res;
+    	
     }
 
     greater() {
-
+    	
+    	this.min = this.res;
+    	
     }
 }
 
